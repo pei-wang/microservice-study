e888a16552a93272562b1cb518d7018e97ca9ea2
Wang pei <519875872@qq.com>
2017-09-03 13:43:07 +0800

    first commit

---
+++ microservice-simple-eurekaServer/src/main/java/com/pei/cloud/microservicesimpleeurekaServer/MicroserviceSimpleEurekaServerApplication.java
@@ -0,0 +1,14 @@
+package com.pei.cloud.microservicesimpleeurekaServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class MicroserviceSimpleEurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroserviceSimpleEurekaServerApplication.class, args);
+    }
+}
---
+++ microservice-simple-eurekaServer/src/test/java/com/pei/cloud/microservicesimpleeurekaServer/MicroserviceSimpleEurekaServerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.pei.cloud.microservicesimpleeurekaServer;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MicroserviceSimpleEurekaServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
