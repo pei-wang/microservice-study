f9ef6b7ce38fb9aab64dddbf3b0115a8a8f044cf
pei wang <519875872@qq.com>
2017-09-04 23:34:26 +0800

    apply feign

--- microservice-simple-consumer-user/src/main/java/com/pei/cloud/microservicesimpleconsumeruser/MicroserviceSimpleConsumerUserApplication.java
+++ microservice-simple-consumer-user/src/main/java/com/pei/cloud/microservicesimpleconsumeruser/MicroserviceSimpleConsumerUserApplication.java
@@ -3,14 +3,18 @@ package com.pei.cloud.microservicesimpleconsumeruser;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @EnableDiscoveryClient
 @SpringBootApplication
+@EnableFeignClients
 public class MicroserviceSimpleConsumerUserApplication {
 
     @Bean
+    @LoadBalanced
     public RestTemplate restTemplate() {
         return new RestTemplate();
     }
---
+++ microservice-simple-consumer-user/src/main/java/com/pei/cloud/microservicesimpleconsumeruser/UserFeignClient.java
@@ -0,0 +1,12 @@
+package com.pei.cloud.microservicesimpleconsumeruser;
+
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@FeignClient(name = "microservice-provider-user")
+public interface UserFeignClient {
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    User findById(@PathVariable("id") Long id);
+}
